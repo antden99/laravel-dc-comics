0bc43072bf57f413e2bdd11f63dbf550c735ee30
antden99 <antoniodenigris@icloud.com>
2024-05-15 16:26:33 +0200

    add function create and store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,7 @@ class ComicController extends Controller
     {
         $comicsList = Comic::all(); //con questa query recupero tutti i campi della tabella comic all'interno del mio DB.
         //dd($comicsList);
-        return view('comics.index',compact('comicsList'));
+        return view('comics.index', compact('comicsList'));
     }
 
     /**
@@ -22,7 +22,7 @@ class ComicController extends Controller
      */
     public function create()
     {
-       return view('comics.create');
+        return view('comics.create');
     }
 
     /**
@@ -30,7 +30,21 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        $data=$request->all();
+        //$data contiene tutti i dari forniti nel form di tipo Post
+        $newComic = new Comic();
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->save(); //ricordati sempre di salvare altrimenti la nuova istanza non viene creata
+
+
+        //dopo aver salvato dobbiamo fare un redirect() perchè altrimenti potremmo incorrere in un loop con il form
+        return to_route('comics.index');
     }
 
     /**
@@ -38,7 +52,7 @@ class ComicController extends Controller
      */
     public function show(Comic $comic)
     {
-        return view('comics.show',compact('comic'));
+        return view('comics.show', compact('comic'));
     }
 
     /**
